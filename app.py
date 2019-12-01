45ddc14bac09b1f992c0d63c9d2f66c31a3aea00
Luca-Dorin Anton <luca-dorin.anton@kcl.ac.uk>
2019-12-01 04:13:45 +0000

    stuff

--- app.py
+++ app.py
@@ -6,22 +6,70 @@ import re
 
 store = json.loads(open('store.json').read())
 
-terms_lst = []
+# name: xxx, category: xxxx, row: xxx, numberOfRows: xxx
 
-for shelf in store['shelves']:
-    terms_lst.append(shelf['cat'])
-    for row in shelf['rows']:
-        terms_lst.append(row)
+@app.route('/search/<cat>', methods=['GET'])
+def search(cat):
+    cat = cat.lower().strip()
+    if cat not in ['all', 'food', 'drinks', 'other']:
+        return jsonify({'error': 'Unkown category!'})
 
-terms_lst = list(dict.fromkeys(terms_lst))
+    res = []
+    if cat == 'all':
+        for shelf in store['shelves']:
+            res.append(shelf['name'])
+            for r in shelf['rows']:
+                res.append({"name": shelf["name"], "category", shelf["category"], "row": int(len(shelf["rows"]/2)), "numberOfRows" : len(shelf["rows"])})
+    else:
+        for shelf in store['shelves']:
+            if shelf['category'].lower().strip() == cat:
+                res.append(shelf['name'])
+                for r in shelf['rows']:
+                    res.append({"name": r, "category", shelf["category"], "row": shelf["rows"].index(r), "numberOfRows" : len(shelf["rows"])})
+    return jsonify(list(dict.fromkeys(res)))
 
 
-
-@app.route('/search/<term>', methods=['GET'])
-def hello_world(term):
+@app.route('/search/<cat>/<term>', methods=['GET'])
+def search_t(term, cat):
+    cat = cat.lower().strip()
     term = term.lower().strip()
     res = []
-    for t in terms_lst:
-        if t.startswith(term):
-            res.append(t)
-    return jsonify(res)
+    if cat not in ['all', 'food', 'drinks', 'other']:
+        return jsonify({'error': 'Unkown category!'})
+
+    if cat == 'all':
+        for shelf in store['shelves']:
+            if shelf['name'].startswith(term):
+                res.append({"name": shelf["name"], "category", shelf["category"], "row": int(len(shelf["rows"]/2)), "numberOfRows" : len(shelf["rows"])})
+            for r in shelf['rows']:
+                if r.startswith(term):
+                    res.append({"name": r, "category", shelf["category"], "row": shelf["rows"].index(r), "numberOfRows" : len(shelf["rows"])})
+    else:
+        for shelf in store['shelves']:
+            if shelf['category'].lower().strip() == cat:
+                if shelf['name'].startswith(term):
+                    res.append(shelf['name'])
+                for r in shelf['rows']:
+                    if r.startswith(term):
+                        res.append(r)
+    return jsonify(list(dict.fromkeys(res)))
+
+
+@app.route('/search-simple/<term>', methods=['GET'])
+def search_simple(term):
+    res = []
+    term = term.lower().strip()
+    for shelf in store['shelves']:
+        if shelf['name'].startswith(term):
+            res.append({"name": shelf["name"], "category", shelf["category"], "row": int(len(shelf["rows"]/2)), "numberOfRows" : len(shelf["rows"])})
+        for r in shelf['rows']:
+            if r.startswith(term):
+                res.append({"name": r, "category", shelf["category"], "row": shelf["rows"].index(r), "numberOfRows" : len(shelf["rows"])})
+    return jsonify(list(dict.fromkeys(res)))
+
+
+
+
+@app.route('/plan', methods=['POST'])
+def plan():
+    return "TEst"
